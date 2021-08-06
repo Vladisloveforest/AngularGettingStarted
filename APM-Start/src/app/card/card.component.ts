e9d2b500a46813d44bcacbb6bec4a7cdaa87e8d9
Vladislove <forest6504@yandex.ru>
2021-08-06 17:22:28 +0300

    added first component

---
+++ APM-Start/src/app/card/card.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  template:`<div class="card"> HERE IS Card component
+   </div>`,
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent {
+
+  constructor() { }
+
+}
