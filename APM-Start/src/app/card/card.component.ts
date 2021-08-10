2f5390aec67ce1b404eaf1b1543eb8ea662c5a5b
Vladislove <forest6504@yandex.ru>
2021-08-10 17:42:27 +0300

    added first component

--- APM-Start/src/app/card/card.component.ts
+++ APM-Start/src/app/card/card.component.ts
@@ -2,12 +2,34 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
-  template:`<div class="card"> HERE IS Card component
-   </div>`,
+  templateUrl:'./card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent {
+
+export class CardComponent implements OnInit {
 
   constructor() { }
 
+  text = 'im i miss something?'
+
+  imgUrl = 'https://i.pinimg.com/originals/84/9b/63/849b636ec25a4f8badbf66acfff2d0c7.jpg'
+
+  ngOnInit() {
+
+  }
+
+  toggleImage() {
+    this.imgUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMMNirtA03z3SH6t4_uzKspyhFHCvo6iFGxA&usqp=CAU"
+    setTimeout(() => {
+      this.imgUrl = 'https://i.pinimg.com/originals/84/9b/63/849b636ec25a4f8badbf66acfff2d0c7.jpg'
+    },3000)
+  }
+
+  changeText(){
+    this.text = "HAHHAHA";
+  }
+
+  onChangeInput(event: any){
+    this.text = event.target.value;
+  }
 }
