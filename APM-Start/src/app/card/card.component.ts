01f58499a85315f33bd50b8ed489a36594474999
Vladislove <forest6504@yandex.ru>
2021-08-16 17:44:34 +0300

    added images on card component

--- APM-Start/src/app/card/card.component.ts
+++ APM-Start/src/app/card/card.component.ts
@@ -10,26 +10,37 @@ export class CardComponent implements OnInit {
 
   constructor() { }
 
-  text = 'im i miss something?'
+  text = 'Card text'
+  disabled = false;
+  textStyle = {
+    color: '#333',
+
+  }
+
+  imgsUrl = ['https://i.pinimg.com/originals/84/9b/63/849b636ec25a4f8badbf66acfff2d0c7.jpg','https://thumbs.dreamstime.com/b/happy-cat-closeup-portrait-funny-smile-cardboard-young-blue-background-102078702.jpg', 'https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZnVubnklMjBwb3J0cmFpdHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvGB2-4k5oSsLiRXmgMFJjQL2hjeuhAcxlsQ&usqp=CAU'
+  ]
 
   imgUrl = 'https://i.pinimg.com/originals/84/9b/63/849b636ec25a4f8badbf66acfff2d0c7.jpg'
-
   ngOnInit() {
 
   }
 
   toggleImage() {
-    this.imgUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMMNirtA03z3SH6t4_uzKspyhFHCvo6iFGxA&usqp=CAU"
-    setTimeout(() => {
-      this.imgUrl = 'https://i.pinimg.com/originals/84/9b/63/849b636ec25a4f8badbf66acfff2d0c7.jpg'
-    },3000)
+
   }
 
-  changeText(){
-    this.text = "HAHHAHA";
-  }
+  onInputChange(){
 
-  onChangeInput(event: any){
-    this.text = event.target.value;
+    if(this.text.length > 10){
+      this.textStyle = {
+        ...this.textStyle,
+        color: 'red'
+
+      }
+    }
+  }
+  changeImg(){
+    this.imgUrl = this.imgsUrl[Math.floor(Math.random()*this.imgsUrl.length)]
   }
 }
