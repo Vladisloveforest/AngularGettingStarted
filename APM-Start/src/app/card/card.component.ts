e1648fffe8692fedbfd71060e1785083fef0fbe7
Vladislove <forest6504@yandex.ru>
2021-08-17 17:10:39 +0300

    added cards from lorem picsum

--- APM-Start/src/app/card/card.component.ts
+++ APM-Start/src/app/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -8,39 +9,14 @@ import { Component, OnInit } from '@angular/core';
 
 export class CardComponent implements OnInit {
 
-  constructor() { }
+  // constructor() { }
 
-  text = 'Card text'
-  disabled = false;
-  textStyle = {
-    color: '#333',
+  @Input() url:string='';
+  @Input() title:string='';
 
-  }
 
-  imgsUrl = ['https://i.pinimg.com/originals/84/9b/63/849b636ec25a4f8badbf66acfff2d0c7.jpg','https://thumbs.dreamstime.com/b/happy-cat-closeup-portrait-funny-smile-cardboard-young-blue-background-102078702.jpg', 'https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZnVubnklMjBwb3J0cmFpdHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvGB2-4k5oSsLiRXmgMFJjQL2hjeuhAcxlsQ&usqp=CAU'
-  ]
 
-  imgUrl = 'https://i.pinimg.com/originals/84/9b/63/849b636ec25a4f8badbf66acfff2d0c7.jpg'
   ngOnInit() {
-
-  }
-
-  toggleImage() {
-
-  }
-
-  onInputChange(){
-
-    if(this.text.length > 10){
-      this.textStyle = {
-        ...this.textStyle,
-        color: 'red'
-
-      }
-    }
-  }
-  changeImg(){
-    this.imgUrl = this.imgsUrl[Math.floor(Math.random()*this.imgsUrl.length)]
+    console.log('card init')
   }
 }
--- APM-Start/src/app/home/welcome.component.ts
+++ APM-Start/src/app/home/welcome.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector:"welcome",
-  templateUrl: './welcome.component.html'
+  templateUrl: './welcome.component.html',
+  styleUrls: ["./welcome.component.css"]
 })
-export class WelcomeComponent {
-  public pageTitle = 'Welcome';
+export class WelcomeComponent implements OnInit {
+
+  imagesUrls:string[] = []
+  public pageTitle = 'Just Random Pictures';
+  showImages:boolean = true;
+  filterItems:string = '';
+  titles: string[] = ['coffee','book','droplets','tower','field','car','mansion','random image','station','field','plant','random image','city','plants','cow','trees','coast',]
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.imagesUrls = ['https://picsum.photos/id/42/300', 'https://picsum.photos/id/24/300', 'https://picsum.photos/id/123/300', 'https://picsum.photos/id/500/300','https://picsum.photos/id/112/300', 'https://picsum.photos/id/111/300', 'https://picsum.photos/id/142/300', 'https://picsum.photos/300','https://picsum.photos/id/800/300', 'https://picsum.photos/id/300/300', 'https://picsum.photos/id/400/300', 'https://picsum.photos/300','https://picsum.photos/id/288/300', 'https://picsum.photos/id/18/300', 'https://picsum.photos/id/200/300', 'https://picsum.photos/id/10/300','https://picsum.photos/id/14/300']
+  }
+  
+
 }
